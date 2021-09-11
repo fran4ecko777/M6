ee9d94e0e7711fd1c349368493d0c7376b72b4c1
kamoe <fran4ecko1990@yandex.ru>
2021-09-11 18:31:06 +0300

    fix_js

--- src/СalorieСounter.js
+++ src/СalorieСounter.js
@@ -2,31 +2,127 @@ import React, { Component } from "react";
 
 class СalorieСounter extends Component {
     state = {
-        age: "",
-        weight: "",
-        height: "",
-        min: 1.2,
-        low: 1.375,
-        medium: 1.55,
-        high: 1.725,
-        max: 1.9,
+        age: 0,
+        weight: 0,
+        height: 0,
+        activity: "",
+        gender: "",
         isValid: false,
+        resultCaloriesNorm: "",
+        resultCaloriesMin: "",
+        resultCaloriesMax:"",
+    }
+
+    checkAllInputsNotEpmty(newState) {
+        if (!newState.age) {
+            return false
+        } else if (!newState.weight) {
+            return false
+        } else if (!newState.height) {
+            return false
+        } else if (!newState.gender) {
+            return false
+        } else if (!newState.activity) {
+            return false
+        }
+        return true
     }
 
     handlerChandeInput = (event) => {
         let name = event.target.name;
         let value = event.target.value;
-        let newState = {};
+        let newState = {...this.state};
+        newState[name] = Number(value);
+        
+        if (value <= 999) {
+            this.setState(newState)
+        } else {
+            return false
+        }
+        let allInputsNotEpmty = this.checkAllInputsNotEpmty(newState)
+        
+        this.setState({
+            isValid: !allInputsNotEpmty ? false : true,
+        })
+    }
+
+    changeValuesInState = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        let newState = {...this.state};
         newState[name] = value;
-        this.setState({newState})
+        this.setState(newState)
+        return newState
+    }
+
+    handlerChandeInputGender = (event) => {
+        let newState = this.changeValuesInState(event)
+        let allInputsNotEpmty = this.checkAllInputsNotEpmty(newState)
+        this.setState({isValid: !allInputsNotEpmty ? false : true})
+    }
+
+    handlerChandeInputActivity = (event) => {
+        let newState = this.changeValuesInState(event)
+        let allInputsNotEpmty = this.checkAllInputsNotEpmty(newState)
+        this.setState({isValid: !allInputsNotEpmty ? false : true})
+    }    
+
+    getGenderCoefficient() {
+        if (this.state.gender === "male") {
+            return 5;
+        } else if (this.state.gender === "female") {
+            return -160;
+        }
+    }
+
+    getCoefficientActivity () {
+        if (this.state.activity === "min") {
+            return 1.2;
+        } else if (this.state.activity === "low") {
+            return 1.375;
+        } else if (this.state.activity === "medium") {
+            return 1.55;
+        } else if (this.state.activity === "high") {
+            return 1.725;
+        } else if (this.state.activity === "max") {
+            return 1.9;
+        }
+    }
+
+    getCaloriesNorm() {
+        let genderCoefficient = this.getGenderCoefficient()
+        let getCoefficientActivity = this.getCoefficientActivity()
+        let age = this.state.age
+        let weight = this.state.weight
+        let height = this.state.height
+        let resultAge = 5 * age;
+        let resultWeight = 10 *  weight;
+        let resultHeight = 6.25 * height
+        let resultCaloriesNorm = Math.round((resultWeight + resultHeight - resultAge + genderCoefficient) * getCoefficientActivity);
+        this.setState({resultCaloriesNorm:resultCaloriesNorm})
+        return resultCaloriesNorm
+    }
+
+    getCaloriesMin() {
+        let resultCaloriesMin = Math.round(this.getCaloriesNorm() * 0.85);
+        this.setState({resultCaloriesMin:resultCaloriesMin})
+        return resultCaloriesMin
+    }
+
+    getCaloriesMax() {
+        let resultCaloriesMax = Math.round(this.getCaloriesNorm() * 1.15);
+        this.setState({resultCaloriesMax:resultCaloriesMax})
+        return resultCaloriesMax
     }
 
     handlerSumbit = (event) => {
         event.preventDefault();
-        const information = new FormData(event.target);
-        console.log(information.get("age"));
-        console.log(information.get("weight"));
-        console.log(information.get("height"));
+        this.getCaloriesNorm()
+        this.getCaloriesMin()
+        this.getCaloriesMax()
+        console.log(this.getCaloriesNorm())
+        console.log(this.getCaloriesMin())
+        console.log(this.getCaloriesMax())
     }
 
     render () {
@@ -37,91 +133,93 @@ class СalorieСounter extends Component {
                     <form onSubmit={this.handlerSumbit}>
                         <h2>Пол</h2>
                         <div className="button_gender">
-                            <button>Мужской</button>
-                            <button>Женский</button>
+                            <h3>Мужчина</h3>
+                            <input type="radio" name="gender" onChange={this.handlerChandeInputGender} value="male"/>
+                            <h3>Женщина</h3>
+                            <input type="radio" name="gender" onChange={this.handlerChandeInputGender} value="female"/>
                         </div>
                         <h3>Физические параметры</h3>
                         <div>
                             <div>
                                 Возраст
-                                <lebe>
+                                <label>
                                     <input
                                         onChange={this.handlerChandeInput} 
-                                        type="text" 
+                                        type="number" 
                                         name="age" 
-                                        placeholder="0" 
-                                        maxlength="3"
+                                        placeholder="0"
                                         className="initial_data"
+                                        value={!this.state.age ? "" : this.state.age}
                                     />
-                                </lebe>
+                                </label>
                                 Вес
-                                <lebe>
+                                <label>
                                     <input 
                                         onChange={this.handlerChandeInput} 
-                                        type="text" 
+                                        type="number" 
                                         name="weight" 
-                                        placeholder="0" 
-                                        maxlength="3"
+                                        placeholder="0"
                                         className="initial_data"
+                                        value={!this.state.weight ? "" : this.state.weight}
                                     />
-                                </lebe>
+                                </label>
                                 Рост
-                                <lebe>
+                                <label>
                                     <input 
                                         onChange={this.handlerChandeInput} 
-                                        type="text" 
+                                        type="number" 
                                         name="height" 
-                                        placeholder="0" 
-                                        maxlength="3"
+                                        placeholder="0"
                                         className="initial_data"
+                                        value={!this.state.height ? "" : this.state.height}
                                     />
-                                </lebe>
+                                </label>
                             </div>
                         </div>
                         <h3>Физическая активность</h3>
                         <div>
                             <div className="radio_input">
                                 <input 
-                                    // onChange={this.handlerChandeInput} 
+                                    onChange={this.handlerChandeInputActivity} 
                                     type="radio" 
                                     name="activity"
-                                    value={this.state.min}
+                                    value='min'
                                 />
                                 Нет физических нагрузок
                             </div>
                             <div className="radio_input">
                                 <input 
-                                    // onChange={this.handlerChandeInput} 
+                                    onChange={this.handlerChandeInputActivity} 
                                     type="radio" 
                                     name="activity"
-                                    value={this.state.low}
+                                    value="low"
                                 />
                                 Низкая физическая активность
                             </div>
                             <div className="radio_input">
                                 <input 
-                                    // onChange={this.handlerChandeInput} 
+                                    onChange={this.handlerChandeInputActivity} 
                                     type="radio" 
                                     name="activity"
-                                    value={this.state.medium}
+                                    value="medium"
                                 />
                                 Средня физическая активность
                             </div>
                             <div className="radio_input">                                
                                 <input 
-                                    // onChange={this.handlerChandeInput} 
+                                    onChange={this.handlerChandeInputActivity} 
                                     type="radio" 
                                     name="activity"
-                                    value={this.state.high}
+                                    value="high"
                                 />
                                 Высокая физическая активность
                             </div>
                             <div className="radio_input">
                                 <input 
-                                    // onChange={this.handlerChandeInput} 
+                                    onChange={this.handlerChandeInputActivity} 
                                     type="radio" 
                                     name="activity"
-                                    value={this.state.max}
+                                    value="max"
                                 />
                                 Очень высокая физическая активность
                             </div>
@@ -131,6 +229,12 @@ class СalorieСounter extends Component {
                             <button>Очистить все поля</button>
                         </div>
                     </form>
+                </div>
+                <div className="сounteiner">
+                    <h2>Ваша норма калорий</h2>
+                    <div>Для снижение веса {this.state.resultCaloriesMin}</div>
+                    <div>Для подержания веса {this.state.resultCaloriesNorm}</div>
+                    <div>Для наборa веса {this.state.resultCaloriesMax}</div>
                 </div>
             </main>
         )
